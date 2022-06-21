8f84f2e747f4292dc709dc898e87473e0e9431e2
blendthink <info@blendthink.dev>
2022-06-21 21:12:39 +0900

    add Log class

---
+++ lib/util/logger.dart
@@ -0,0 +1,13 @@
+class Log {
+  void w(Object? o) => print('\x1B[33m$o\x1B[0m');
+  void e(Object? o) {
+    final message = '''
+An unexpected error occurred.
+Consider creating an issue on https://github.com/blendthink/falcon/issues/new.
+
+$o''' ;
+    print('\x1B[31m$message\x1B[0m');
+  }
+}
+
+final log = Log();
